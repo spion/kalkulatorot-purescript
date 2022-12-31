c802992d12a6af31868838f5e825879e3d67f3e4
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2022-12-31 05:36:51 +0000

    initial commit

---
+++ src/Main.purs
@@ -0,0 +1,30 @@
+module Main where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Exception (throw)
+import React.Basic.DOM as R
+import React.Basic.DOM.Client (createRoot, renderRoot)
+import React.Basic.Hooks (Component, component)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
+
+main :: Effect Unit
+main = do
+  doc <- document =<< window
+  root <- getElementById "root" $ toNonElementParentNode doc
+  case root of
+    Nothing -> throw "Could not find root."
+    Just container -> do
+      reactRoot <- createRoot container
+      app <- mkApp
+      renderRoot reactRoot (app {})
+
+mkApp :: Component {}
+mkApp = do
+  component "App" \_ -> React.do
+    pure (R.text "Hello!")
